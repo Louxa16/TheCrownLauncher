b7ff1ea3e6ef892970dc2fd64f1df320e920e7d4
Louxa16 <desscraft16@gmail.com>
2023-07-23 16:28:24 -0400

    v1.1.0
    noice

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://files.galaxynetwork.studio/Crown/distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://files.galaxynetwork.studio/Louxa/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
